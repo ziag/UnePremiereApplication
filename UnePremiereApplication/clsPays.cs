733309a49520f3b6abdbdd555ea4d1b751fb0c27
ziag <ziag@yahoo.com>
2016-03-13 18:35:07 -0400

    C,est un debut

---
+++ UnePremiereApplication/clsPays.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clsPays
+{
+    public class Pays
+    {
+        public string NewDataSet { set; get; }
+        public string Table { set; get; }
+        public string Name { set; get; }
+    }
+}
